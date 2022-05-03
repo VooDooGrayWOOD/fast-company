2228b2a97820b169c18a7b22c537b29c16f14e02
VooDooGrayWOOD <voodoograywood@gmail.com>
2022-05-03 22:25:51 +0700

    add validation on project

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import TextField from '../components/textField'
+import { validator } from '../utils/validator'
 
 const Login = () => {
-    return <h1>Login</h1>
+    const [data, setData] = useState({ email: '', password: '' })
+    const [errors, setErrors] = useState({})
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }))
+    }
+
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: 'Электронная почта обязательна для заполнения'
+            },
+            isEmail: { message: 'Email введён не корректно' }
+        },
+        password: {
+            isRequired: {
+                message: 'Пароль обязателен для заполнения'
+            },
+            isCapitalSymbol: {
+                message: 'Пароль должен содеражать хотя бы одну заглавную букву'
+            },
+            isContainDigit: {
+                message: 'Пароль должен содеражать хотя бы одну цифру'
+            },
+            min: {
+                message:
+                    'Пароль должен состоять минимум из 8 (восьми) символов',
+                value: 8
+            }
+        }
+    }
+
+    useEffect(() => {
+        validate()
+    }, [data])
+
+    const validate = () => {
+        const errors = validator(data, validatorConfig)
+        setErrors(errors)
+        return Object.keys(errors).length === 0
+    }
+
+    const isValid = Object.keys(errors).length === 0
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const isValid = validate()
+        if (!isValid) return
+        console.log(data)
+    }
+
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 shadow p-4">
+                    <h3 className="mb-4">Login</h3>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            label="Электронная почта"
+                            onChange={handleChange}
+                            name="email"
+                            value={data.email}
+                            error={errors.email}
+                        />
+                        <TextField
+                            label="Пароль"
+                            type="password"
+                            onChange={handleChange}
+                            name="password"
+                            value={data.password}
+                            error={errors.password}
+                        />
+                        <button
+                            type="submit"
+                            disabled={!isValid}
+                            className="btn btn-primary w-100 mx-auto"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
