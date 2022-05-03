759779c32ac66876122ce8e217daaa550b12dbcb
VooDooGrayWOOD <voodoograywood@gmail.com>
2022-05-03 23:05:00 +0700

    change userPage.jsx

--- src/components/userPage.jsx
+++ src/components/userPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import API from '../api/index'
+import PropTypes from 'prop-types'
+import QualitiesList from './qualitiesList'
 
 const UserPage = () => {
     const history = useHistory()
@@ -16,19 +18,30 @@ const UserPage = () => {
         history.replace('/users')
     }
 
-    return (
-        <>
-            {user ? <h2>{user.name}</h2> : `Loading...`}
-            <button
-                className="btn btn-outline-success btn-sm m-2"
-                onClick={() => {
-                    handleAllUsers()
-                }}
-            >
-                Все Пользователи
-            </button>
-        </>
-    )
+    if (user) {
+        return (
+            <div>
+                <h1> {user.name}</h1>
+                <h2>Профессия: {user.profession.name}</h2>
+                <QualitiesList qualities={user.qualities} />
+                <p>completedMeetings: {user.completedMeetings}</p>
+                <h2>Rate: {user.rate}</h2>
+                <button
+                    onClick={handleAllUsers}
+                    className="btn btn-outline-success btn-sm m-2"
+                >
+                    {' '}
+                    Все Пользователи
+                </button>
+            </div>
+        )
+    } else {
+        return <h1>Loading</h1>
+    }
+}
+
+UserPage.propTypes = {
+    userId: PropTypes.string.isRequired
 }
 
 export default UserPage
